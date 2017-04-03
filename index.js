014781373d85c99a04a711dffc5eed5dac1f1953
SOS1617_ipl <ipeluaga@gmail.com>
2017-04-03 15:05:33 +0000

    api key updates

--- index.js
+++ index.js
@@ -6,11 +6,26 @@ var apiNacho = require('./apiNacho.js');
 var apiAlberto = require('./apiAlberto.js');
 var apiAntonio = require('./apiAntonio.js');
 
-var port = (process.env.PORT || 10000); 
-var BASE_API_PATH = "/api/v1"; 
+var port = (process.env.PORT || 10000);
+var BASE_API_PATH = "/api/v1";
 var app = express();
 
- 
+//Función que comprueba la clave especificada en la URL (apiKey):
+var ourKey = "cinco";
+var checkKey = function (req, res) {
+    if (!req.params.apikey) {
+        console.log("ERROR: No key was specified");
+        res.sendStatus(401);
+        return false;
+    }
+    else if (req.params.apikey !== ourKey) {
+        console.log("ERROR: Wrong key");
+        res.sendStatus(403)
+        return false;
+    }
+    return true;
+};
+
 
 app.use("/api/v1/tests", express.static(path.join(__dirname, "public/tests.html")));
 
@@ -18,8 +33,7 @@ app.use(bodyParser.json()); //use default json enconding/decoding
 app.use(helmet()); //improve security
 
 
-apiNacho.register(app,port,BASE_API_PATH);  
-apiAlberto.register(app,port,BASE_API_PATH);  
-apiAntonio.register(app,port,BASE_API_PATH); 
 
-
+apiNacho.register(app, port, BASE_API_PATH, checkKey);
+apiAlberto.register(app, port, BASE_API_PATH, checkKey);
+apiAntonio.register(app, port, BASE_API_PATH, checkKey);
