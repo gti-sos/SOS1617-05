62091253a66fd46e727253395f6bdbf4994d37eb
SOS1617_ipl <ipeluaga@gmail.com>
2017-04-25 22:58:29 +0000

    deploy to keroku

---
+++ public/frontends/nacho/test/heroku/T01-LoadResources.js
@@ -0,0 +1,14 @@
+//Test para cargar datos
+
+describe('Resources is loaded', function(){
+    it('should show a bunch of data',function(){//en el it describimos que debe pasar 
+       //TO DO--->Tengo que cambiarlo para heroku
+        browser.get('https://sos1617-05.herokuapp.com/api/v1/frontendNacho/#!/');//El puerto es 8080 ya que estoy lanzando el navegador fantasma sobre lo que está en c9
+        //me devuelve toda la lista de reccursos que hay.
+        var electionsVotingStats = elemet.all(by.repeater('economicSituation in economicSituationStats'));//genera un array seleccionando todos los elementos de la pagina renderizada(element.all), seleccionando por una directiva (by.repeater) 
+        //El tamaño del array debe ser mayor que x:
+        expect(electionsVotingStats.count()).toBeGreaterThan(3);
+        
+    });
+    
+});
