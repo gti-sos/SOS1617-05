53f6585700dfe558e78fb60c312021dbd1ed8ba4
SOS1617_ipl <ipeluaga@gmail.com>
2017-04-26 00:23:00 +0000

    deploy to keroku

--- public/frontends/nacho/results-list-ctrl.js
+++ public/frontends/nacho/results-list-ctrl.js
@@ -2,7 +2,7 @@
 
 angular
     .module("ResultsManagerApp") //No lleva [] porque no se está creando la App, si no que se está solicitando
-    .controller("ListCtrl", ["$scope", "$http", function($scope, $http) { //$scope es un módulo con el que accedemos al modelo, $http es un módulo que permite hacer peticiones a la API, es decir, conecta con el backend -->
+    .controller("ResultsListCtrl", ["$scope", "$http", function($scope, $http) { //$scope es un módulo con el que accedemos al modelo, $http es un módulo que permite hacer peticiones a la API, es decir, conecta con el backend -->
         console.log(" List Controller initialized");
         //Sería interesante concatenar la apikey a la URL en cada método por si hubiera que agregarle algo a la URL, no???
         $scope.url = "https://sos1617-05.herokuapp.com/api/v1/elections-voting-stats";
---
+++ public/manager-app.js
@@ -0,0 +1,25 @@
+//[]--->carga modulos en este caso cargaremos angular-root-->ngRoute
+angular.module("ManagerApp", ["ngRoute"]).config(function($routeProvider) {
+
+    //Cuando un usuario acceda al recurso "/"" carge la vista list.html
+    $routeProvider
+        .when("/", {
+            templateUrl: "main.html"
+        })
+        .when("/elections-voting-results", {
+            templateUrl: "frontends/nacho/list.html",
+            controller: "ResultsListCtrl" //le asocio el objeto registrado en angular-->ListCtrl
+        }).when("/elections-voting-results/:province", {
+            templateUrl: "frontends/nacho/edit.html",
+            controller: "ResultsEditCtrl" //Se asocia a un objeto que se supone que ya está registrado en Angular (el controlador)
+
+        }).when("/economic-stats", {
+            templateUrl: "frontends/alberto/list.html",
+            controller: "ListCtrl" //Se asocia a un objeto que se supone que ya está registrado en Angular (el controlador)
+        }).when("/economic-stats/:province", {
+            templateUrl: "frontends/albertoedit.html",
+            controller: "EditCtrl"
+        });
+    //Para crear el modulo.Lista de dependencias que quieres cargar,nombre de ng-app 
+    console.log("App initialized and configured");
+});
