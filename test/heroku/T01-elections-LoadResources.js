9980eca5446937013f26aa0674d7956a822b8382
SOS1617_ipl <ipeluaga@gmail.com>
2017-05-06 07:37:02 +0000

    deploy to keroku

--- test/heroku/T01-elections-LoadResources.js
+++ test/heroku/T01-elections-LoadResources.js
@@ -6,7 +6,7 @@ describe('Resources is loaded', function() {
         browser.get('https://sos1617-05.herokuapp.com/#!/elections-voting-stats'); //El puerto es 8080 ya que estoy lanzando el navegador fantasma sobre lo que está en c9
         //me devuelve toda la lista de reccursos que hay.
         browser.driver.sleep(10000);
-        var electionsVotingStats = element.all(by.repeater('votingResults in electionsVotingStats')); //genera un array seleccionando todos los elementos de la pagina renderizada(element.all), seleccionando por una directiva (by.repeater) 
+        var electionsVotingStats = element.all(by.repeater('result in results')); //genera un array seleccionando todos los elementos de la pagina renderizada(element.all), seleccionando por una directiva (by.repeater) 
         //El tamaño del array debe ser mayor que x:
         expect(electionsVotingStats.count()).toBeGreaterThan(3);
 
