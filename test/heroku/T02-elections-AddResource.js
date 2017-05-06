824a18cbebc45ecf748ae17a8a254866ce79360e
SOS1617_ipl <ipeluaga@gmail.com>
2017-05-06 19:02:51 +0000

    deploy to keroku

--- test/heroku/T02-elections-AddResource.js
+++ test/heroku/T02-elections-AddResource.js
@@ -1,13 +1,13 @@
 describe('Add voting results', function() {
     it('should add a votingResults', function() {
-        
+
         //EL PROBLEMA ESTÁ EN QUE AL ACCEDER A ESTA RUTA NO SE MUESTRA NINGÚN RECURSO!! PONER QUE CADA VEZ QUE SE ACCEDA APAREZCAN RESURSOS
         browser.get('https://sos1617-05.herokuapp.com/#!/elections-voting-stats');
 
         element.all(by.repeater('result in results'))
             .then(function(initialelectionsVotingStats) {
                 browser.driver.sleep(10000);
-                console.log(initialelectionsVotingStats);//TANTO ESTE COMO EL DEL ÚLTIMO CALL BACK SON ARRAYS VACÍOS
+                console.log(initialelectionsVotingStats); //TANTO ESTE COMO EL DEL ÚLTIMO CALL BACK SON ARRAYS VACÍOS
                 element(by.model('newResult.province')).sendKeys('Sevillaaaa');
                 element(by.model('newResult.year')).sendKeys('2015');
                 element(by.model('newResult.pp')).sendKeys('2');
@@ -16,7 +16,7 @@ describe('Add voting results', function() {
                 element(by.model('newResult.cs')).sendKeys('2');
 
                 element(by.buttonText('Add')).click().then(function() {
-                    element(by.buttonText('Aceptar')).click()
+                    ptor.actions().sendKeys(protractor.Key.ENTER).perform();
                     console.log("CLICK SOBRE BOTÓN Add OKAY!");
                     element.all(by.repeater('result in results'))
                         .then(function(electionsVotingStats) {
