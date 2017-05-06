8024b5063c3d264eb85fc5e10bfe79e38647e6ff
SOS1617_ipl <ipeluaga@gmail.com>
2017-05-06 18:31:00 +0000

    deploy to keroku

--- public/frontends/nacho/results-list-ctrl.js
+++ public/frontends/nacho/results-list-ctrl.js
@@ -283,7 +283,7 @@ angular
         $scope.setPage = function(pageNo) {
 
             var pages = (Math.floor($scope.results.length / $scope.limit)) + 1;
-            if (pageNo < pages) {
+            if (pageNo <= pages) {
                 console.log("PÁGINAS: ",$scope.results.length , $scope.limit,pages);
                 $scope.currentPage = pageNo;
             }
