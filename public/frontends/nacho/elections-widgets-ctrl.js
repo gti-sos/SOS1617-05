ec03c071ae6de2698b0a71c6b85c50eb8653e477
SOS1617_ipl <ipeluaga@gmail.com>
2017-05-09 18:37:51 +0000

    deploy to keroku

--- public/frontends/nacho/elections-widgets-ctrl.js
+++ public/frontends/nacho/elections-widgets-ctrl.js
@@ -56,7 +56,7 @@ angular
                         }
                     },
                     tooltip: {
-                        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> <br/>',
+                        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.x}</b> ({point.percentage:.0f}%)<br/>',
                         shared: true
                     },
                     plotOptions: {
