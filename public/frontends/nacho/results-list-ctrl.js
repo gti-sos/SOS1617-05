eacc19c7cfa650d0ed1abda7506f7dca50dc3486
SOS1617_ipl <ipeluaga@gmail.com>
2017-05-09 20:24:34 +0000

    deploy to keroku

--- public/frontends/nacho/results-list-ctrl.js
+++ public/frontends/nacho/results-list-ctrl.js
@@ -207,33 +207,33 @@ angular
             var numberOfPages;
             //los parámetros especificados (no tienen por qué ser los 6) se acoplan a la URL y se hace un get. Se deben mostrar los que cumplan eso!!
             var params = "";
-            var ref = 0;
+            var ref = 6;
             //Diferencias entre usar == y === ?
             if ($scope.newResult.province !== undefined && $scope.newResult.province !== "") {
                 params = params + "&province=" + $scope.newResult.province;
-                ref = ref + 1;
+                ref = ref - 1;
             }
             if ($scope.newResult.year !== undefined && $scope.newResult.year !== "") {
                 params = params + "&year=" + $scope.newResult.year;
-                ref = ref + 1;
+                ref = ref - 1;
             }
             if ($scope.newResult.pp !== undefined && $scope.newResult.pp !== "") {
                 params = params + "&pp=" + $scope.newResult.pp;
-                ref = ref + 1;
+                ref = ref - 1;
             }
             if ($scope.newResult.podemos !== undefined && $scope.newResult.podemos !== "") {
                 params = params + "&podemos=" + $scope.newResult.podemos;
-                ref = ref + 1;
+                ref = ref - 1;
             }
             if ($scope.newResult.psoe !== undefined && $scope.newResult.psoe !== "") {
                 params = params + "&psoe=" + $scope.newResult.psoe;
-                ref = ref + 1;
+                ref = ref - 1;
             }
             if ($scope.newResult.cs !== undefined && $scope.newResult.cs !== "") {
                 params = params + "&cs=" + $scope.newResult.cs;
-                ref = ref + 1;
+                ref = ref - 1;
             }
-            if (ref == 6) {
+            if (ref == 0) {
                 Materialize.toast('<h4>You must fill at least one field. </h4>', 1500);
             }
 
