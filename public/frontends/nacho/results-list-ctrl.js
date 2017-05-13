af9ced49bc0e9e48a1baa2bc32d2052ec08f7ac6
SOS1617_ipl <ipeluaga@gmail.com>
2017-05-13 17:02:12 +0000

    deploy to keroku

--- public/frontends/nacho/results-list-ctrl.js
+++ public/frontends/nacho/results-list-ctrl.js
@@ -279,7 +279,7 @@ angular
             }
             console.log("ESTÁ EN FUNCIÓN PAGINATION");
             $http
-                .get($scope.url + "?apikey=" + $scope.apikey + limit + offset) //Aquí se realizan los 4 método de API: get, post, put, delete
+                .get($scope.url + "?apikey=" + $scope.apikey + limit + "?offset=" + offset) //Aquí se realizan los 4 método de API: get, post, put, delete
                 .then(function(response) { // Cuando termine de recibir los datos (then) ejecuta el callback
                     console.log("GET collection (pagination) a url: ", $scope.url + "?apikey=" + $scope.apikey + limit + offset);
                     $scope.data = JSON.stringify(response.data, null, 2); // null,2 sirve para renderizar el JSON, que lo muestre bonito, etc...
