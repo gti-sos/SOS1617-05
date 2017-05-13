422b91a6593006893c586b3a0a687029871c7140
SOS1617_ipl <ipeluaga@gmail.com>
2017-05-13 08:54:22 +0000

    deploy to keroku

--- public/frontends/nacho/results-list-ctrl.js
+++ public/frontends/nacho/results-list-ctrl.js
@@ -285,7 +285,7 @@ angular
                         console.log("GET collection (pagination)");
                         $scope.data = JSON.stringify(response.data, null, 2); // null,2 sirve para renderizar el JSON, que lo muestre bonito, etc...
                         $scope.results = response.data;
-                        console.log($scope.results);
+                        console.log("Array obtenido en pagination() con offset ", offset, ": ", $scope.results);
                         /*if (response.status === 200 || response.status === 201) {
                             Materialize.toast('Successful action. ', 1200);
                         }*/
