dac25e932323547f9e48b2c8c2a49de1f5e894c5
SOS1617_ipl <ipeluaga@gmail.com>
2017-05-14 18:05:06 +0000

    deploy to keroku

--- public/frontends/nacho/results-list-ctrl.js
+++ public/frontends/nacho/results-list-ctrl.js
@@ -295,14 +295,14 @@ angular
                 limit = "&limit=" + $scope.limit;
                 offset = (pageNo - 1) * $scope.limit;
             }
-            console.log("ESTÁ EN FUNCIÓN PAGINATION");
+            console.log("ESTÁ EN FUNCIÓN setPage(pageNo)");
             $http
                 .get($scope.url + "?apikey=" + $scope.apikey + limit + "&offset=" + offset) //Aquí se realizan los 4 método de API: get, post, put, delete
                 .then(function(response) { // Cuando termine de recibir los datos (then) ejecuta el callback
                     console.log("GET collection (pagination) a url: ", $scope.url + "?apikey=" + $scope.apikey + limit + "&offset=" + offset);
                     $scope.data = JSON.stringify(response.data, null, 2); // null,2 sirve para renderizar el JSON, que lo muestre bonito, etc...
                     $scope.results = response.data;
-                    console.log("Array obtenido en pagination() con offset ", offset, " y limmit ", $scope.limit, ": ", $scope.results + " ...FIN ARRAY");
+                    //console.log("Array obtenido en pagination() con offset ", offset, " y limmit ", $scope.limit, ": ", $scope.results + " ...FIN ARRAY");
                     /*if (response.status === 200 || response.status === 201) {
                         Materialize.toast('Successful action. ', 1200);
                     }*/
@@ -334,7 +334,7 @@ angular
                 res.push(i);
             }
             console.log("--------------ENTRÓ A FUNCIÓN DE CREACIÓN DE RANGO: ", res, "-----------");
-            return res;
+            $scope.pagesRange = res;
         };
 
 
