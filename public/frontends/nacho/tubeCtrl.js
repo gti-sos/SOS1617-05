e72a9756abbb1f06b59c11a03c0dcec89d6f3f10
SOS1617_ipl <ipeluaga@gmail.com>
2017-05-25 16:56:36 +0000

    deploy to keroku

--- public/frontends/nacho/tubeCtrl.js
+++ public/frontends/nacho/tubeCtrl.js
@@ -30,161 +30,162 @@ angular
         //Aux. Functions:
         var provincesCoords = [{
             'province': 'Almería',
-            'latitud': '36,84016',
-            'longitud': '-2,467922'
+            'latitud': '36.84016',
+            'longitud': '-2.467922'
         }, {
             'province': 'Cádiz',
-            'latitud': '36,52969',
-            'longitud': '-6,292657'
+            'latitud': '36.52969',
+            'longitud': '-6.292657'
         }, {
             'province': 'Córdoba',
-            'latitud': '37,88473',
-            'longitud': '-4,779152'
+            'latitud': '37.88473',
+            'longitud': '-4.779152'
         }, {
             'province': 'Granada',
-            'latitud': '37,17649',
-            'longitud': '-3,597929'
+            'latitud': '37.17649',
+            'longitud': '-3.597929'
         }, {
             'province': 'Huelva',
-            'latitud': '37,2571',
-            'longitud': '-6,949555'
+            'latitud': '37.2571',
+            'longitud': '-6.949555'
         }, {
             'province': 'Jaén',
-            'latitud': '37,76574',
-            'longitud': '-3,789518'
+            'latitud': '37.76574',
+            'longitud': '-3.789518'
         }, {
             'province': 'Málaga',
-            'latitud': '36,71965',
-            'longitud': '-4,420019'
+            'latitud': '36.71965',
+            'longitud': '-4.420019'
         }, {
             'province': 'Sevilla',
-            'latitud': '37,38264',
-            'longitud': '-5,996295'
+            'latitud': '37.38264',
+            'longitud': '-5.996295'
         }, {
             'province': 'Huesca',
-            'latitud': '42,1401',
-            'longitud': '-0,408898'
+            'latitud': '42.1401',
+            'longitud': '-0.408898'
         }, {
             'province': 'Teruel',
-            'latitud': '40,34411',
-            'longitud': '-1,10691'
+            'latitud': '40.34411',
+            'longitud': '-1.10691'
         }, {
             'province': 'Zaragoza',
-            'latitud': '41,65629',
-            'longitud': '-0,8765379'
+            'latitud': '41.65629',
+            'longitud': '-0.8765379'
         }, {
             'province': 'Albacete',
-            'latitud': '38,99765',
-            'longitud': '-1,86007'
+            'latitud': '38.99765',
+            'longitud': '-1.86007'
         }, {
             'province': 'Ciudad Real',
-            'latitud': '38,9861',
-            'longitud': '-3,927263'
+            'latitud': '38.9861',
+            'longitud': '-3.927263'
         }, {
             'province': 'Cuenca',
-            'latitud': '40,07183',
-            'longitud': '-2,134005'
+            'latitud': '40.07183',
+            'longitud': '-2.134005'
         }, {
             'province': 'Guadalajara',
-            'latitud': '40,62981',
-            'longitud': '-3,166493'
+            'latitud': '40.62981',
+            'longitud': '-3.166493'
         }, {
             'province': 'Toledo',
-            'latitud': '39,85678',
-            'longitud': '-4,024476'
+            'latitud': '39.85678',
+            'longitud': '-4.024476'
         }, {
             'province': 'Ávila',
-            'latitud': '40,65642',
-            'longitud': '-4,700323'
+            'latitud': '40.65642',
+            'longitud': '-4.700323'
         }, {
             'province': 'Burgos',
-            'latitud': '42,34087',
-            'longitud': '-3,699731'
+            'latitud': '42.34087',
+            'longitud': '-3.699731'
         }, {
             'province': 'León',
-            'latitud': '42,59988',
-            'longitud': '-5,571752'
+            'latitud': '42.59988',
+            'longitud': '-5.571752'
         }, {
             'province': 'Palencia',
-            'latitud': '42,01246',
-            'longitud': '-4,531175'
+            'latitud': '42.01246',
+            'longitud': '-4.531175'
         }, {
             'province': 'Salamanca',
-            'latitud': '40,96497',
-            'longitud': '-5,663047'
+            'latitud': '40.96497',
+            'longitud': '-5.663047'
         }, {
             'province': 'Segovia',
-            'latitud': '40,94943',
-            'longitud': '-4,119209'
+            'latitud': '40.94943',
+            'longitud': '-4.119209'
         }, {
             'province': 'Soria',
-            'latitud': '41,7636',
-            'longitud': '-2,464921'
+            'latitud': '41.7636',
+            'longitud': '-2.464921'
         }, {
             'province': 'Valladolid',
-            'latitud': '41,65295',
-            'longitud': '-4,728388'
+            'latitud': '41.65295',
+            'longitud': '-4.728388'
         }, {
             'province': 'Zamora',
-            'latitud': '41,50368',
-            'longitud': '-5,743778'
+            'latitud': '41.50368',
+            'longitud': '-5.743778'
         }, {
             'province': 'Barcelona',
-            'latitud': '41,38792',
-            'longitud': '2,169919'
+            'latitud': '41.38792',
+            'longitud': '2.169919'
         }, {
             'province': 'Girona',
-            'latitud': '41,9818',
-            'longitud': '2,8237'
+            'latitud': '41.9818',
+            'longitud': '2.8237'
         }, {
             'province': 'Lleida',
-            'latitud': '41,61415',
-            'longitud': '0,6257825'
+            'latitud': '41.61415',
+            'longitud': '0.6257825'
         }, {
             'province': 'Tarragona',
-            'latitud': '41,11866',
-            'longitud': '1,24533'
+            'latitud': '41.11866',
+            'longitud': '1.24533'
         }, {
             'province': 'Ceuta',
-            'latitud': '35,88829',
-            'longitud': '-5,316195'
+            'latitud': '35.88829',
+            'longitud': '-5.316195'
         }, {
             'province': 'Melilla',
-            'latitud': '35,29234',
-            'longitud': '-2,938794'
+            'latitud': '35.29234',
+            'longitud': '-2.938794'
         }, {
             'province': 'Badajoz',
-            'latitud': '38,8786',
-            'longitud': '-6,970284'
+            'latitud': '38.8786',
+            'longitud': '-6.970284'
         }, {
             'province': 'Cáceres',
-            'latitud': '39,47618',
-            'longitud': '-6,37076'
+            'latitud': '39.47618',
+            'longitud': '-6.37076'
         }, {
             'province': 'Lugo',
-            'latitud': '43,01208',
-            'longitud': '-7,555851'
+            'latitud': '43.01208',
+            'longitud': '-7.555851'
         }, {
             'province': 'Ourense',
-            'latitud': '42,34001',
-            'longitud': '-7,864641'
+            'latitud': '42.34001',
+            'longitud': '-7.864641'
         }, {
             'province': 'Pontevedra',
-            'latitud': '42,43362',
-            'longitud': '-8,648053'
+            'latitud': '42.43362',
+            'longitud': '-8.648053'
         }, {
             'province': 'Madrid',
-            'latitud': '40,41669',
-            'longitud': '-3,700346'
+            'latitud': '40.41669',
+            'longitud': '-3.700346'
         }, {
             'province': 'Murcia',
-            'latitud': '37,98344',
-            'longitud': '-1,12989'
+            'latitud': '37.98344',
+            'longitud': '-1.12989'
         }, {
             'province': 'Valencia',
-            'latitud': '39,47024',
-            'longitud': '-0,3768049'
+            'latitud': '39.47024',
+            'longitud': '-0.3768049'
         }];
+
 
         function lat(province) {
             console.log("ENTRADA lat(): ", province);
