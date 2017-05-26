3ed3f52c53d1f7c073d3e094068556447318809f
SOS1617_ipl <ipeluaga@gmail.com>
2017-05-26 09:40:11 +0000

    deploy to keroku

--- public/frontends/nacho/usCtrl.js
+++ public/frontends/nacho/usCtrl.js
@@ -19,8 +19,13 @@ angular
         $scope.data = {};
         var data = {};
 
-        //US Elections data (external API): qué almacenar??? contar cuantos participantes (estados) hay para cada partido y representar dichas cantidades en el pie/donut???
+        //-US Elections data (external API): qué almacenar??? contar cuantos participantes (estados) hay para cada partido y representar dichas cantidades en el pie/donut???
         //Entiendo que cada recurso informa del partido que ganó (obtuvo más escaños) en el estado en el que se indica, RELACIONAR ESO CON LO EQUIVALENTE AQUÍ!!
+        //-En USA hay sólo dos partidos...republicano y demócrata. Digamos que los pongo como socialista y conservador, y luego agrupo los pares de partidos
+        //españoles de cada lado (izquierda psoe y podemos) y derecha (pp y C's). Una vez hecho esto, representarlo y COMPARAR los tamaños de cada movimiento
+        //entre los 2 paises.
+        //HAY 50 ESTADOS EN USA     
+
         $scope.dataUs = {};
         var dataUs = {};
 
@@ -52,18 +57,72 @@ angular
                         $scope.dataUs = dataUs;
 
                         for (var i = 0; i < res.data.length; i++) {
-                        
+
                             console.log();
                         }
 
                         console.log("Controller intialized");
 
-//Posibles librerías: morris.js , chartist.js , 
-//También se podría usar una cosa como: http://plottablejs.org/examples/mondrian/ así viendo la diferencia entre las areas se apreciaría de anera muy gráfica la diferencia en volumen que hay entre unas elecciones aquí y unas alli.
+                        //Posibles librerías: morris.js , chartist.js , 
+                        //También se podría usar una cosa como: http://plottablejs.org/examples/mondrian/ así viendo la diferencia entre las areas se apreciaría de anera muy gráfica la diferencia en volumen que hay entre unas elecciones aquí y unas alli.
 
-
-
-
+                        //Highchart: (tipo treemap, completamente diferente a los otros tipos YA usado de esta librería)
+                        Highcharts.chart('containerUs', {
+                            series: [{
+                                type: "treemap",
+                                layoutAlgorithm: 'stripes',
+                                alternateStartingDirection: true,
+                                levels: [{
+                                    level: 1,
+                                    layoutAlgorithm: 'sliceAndDice',
+                                    dataLabels: {
+                                        enabled: true,
+                                        align: 'left',
+                                        verticalAlign: 'top',
+                                        style: {
+                                            fontSize: '15px',
+                                            fontWeight: 'bold'
+                                        }
+                                    }
+                                }],
+                                data: [{
+                                    id: 'A',
+                                    name: 'Conservatism ', //Derecha
+                                    color: "#0080FF"
+                                }, {
+                                    id: 'B',
+                                    name: 'Social liberalism', //Izquierda
+                                    color: "#FA5858"
+                                }, {
+                                    name: 'Partido Popular',
+                                    parent: 'A',
+                                    value: 5
+                                }, {
+                                    name: 'PSOE',
+                                    parent: 'B',
+                                    value: 5
+                                }, {
+                                    name: 'Podemos',
+                                    parent: 'B',
+                                    value: 3
+                                }, {
+                                    name: 'C',
+                                    parent: 'A',
+                                    value: 4
+                                }, {
+                                    name: 'Republican Party', //Derecha
+                                    parent: 'A',
+                                    value: 4
+                                }, {
+                                    name: 'Democratic Party', //Izquierda
+                                    parent: 'B',
+                                    value: 10
+                                }]
+                            }],
+                            title: {
+                                text: 'Parties in the US and in Spain'
+                            }
+                        });
                     });
             });
     }]);
