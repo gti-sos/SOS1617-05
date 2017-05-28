238e9d2bdb21cadd201af6da04c42a044809bc7d
SOS1617_ipl <ipeluaga@gmail.com>
2017-05-28 02:38:45 +0000

    deploy to keroku

--- public/frontends/nacho/chicagoCtrl.js
+++ public/frontends/nacho/chicagoCtrl.js
@@ -55,32 +55,49 @@ angular
                         dataChicago = res.data;
                         $scope.dataChicago = dataChicago;
                         for (var i = 0; i < res.data.length; i++) { //HAY ALGÚN PARTIDO QUE NO SEA ALGUNO DE ESTOS 2 EN EL JSON QUE SE DEVUELVE???
-                            console.log("dato: ",$scope.dataChicago[i]);
                             if ($scope.dataChicago[i]._suffix == 'ST') {
+                                                            console.log("dato: ",$scope.dataChicago[i]);
+
                                 $scope.st = $scope.st + Number($scope.dataChicago[i]._amount_paid);
                             }
                             else if ($scope.dataChicago[i]._suffix == 'AVE') {
+                                                            console.log("dato: ",$scope.dataChicago[i]);
+
                                 $scope.ave = $scope.ave + Number($scope.dataChicago[i]._amount_paid);
                             }
                             else if ($scope.dataChicago[i]._suffix == 'PL') {
+                                                            console.log("dato: ",$scope.dataChicago[i]);
+
                                 $scope.pl = $scope.pl + Number($scope.dataChicago[i]._amount_paid);
                             }
                             else if ($scope.dataChicago[i]._suffix == 'BLVD') {
+                                                            console.log("dato: ",$scope.dataChicago[i]);
+
                                 $scope.blvd = $scope.blvd + Number($scope.dataChicago[i]._amount_paid);
                             }
                             else if ($scope.dataChicago[i]._suffix == 'CT') {
+                                                            console.log("dato: ",$scope.dataChicago[i]);
+
                                 $scope.ct = $scope.ct + Number($scope.dataChicago[i]._amount_paid);
                             }
                             else if ($scope.dataChicago[i]._suffix == 'PKWY') {
+                                                            console.log("dato: ",$scope.dataChicago[i]);
+
                                 $scope.pkwy = $scope.pkwy + Number($scope.dataChicago[i]._amount_paid);
                             }
                             else if ($scope.dataChicago[i]._suffix == 'RD') {
+                                                            console.log("dato: ",$scope.dataChicago[i]);
+
                                 $scope.rd = $scope.rd + Number($scope.dataChicago[i]._amount_paid);
                             }
                             else if ($scope.dataChicago[i]._suffix == 'TER') {
+                                                            console.log("dato: ",$scope.dataChicago[i]);
+
                                 $scope.ter = $scope.ter + Number($scope.dataChicago[i]._amount_paid);
                             }
                             else if ($scope.dataChicago[i]._suffix == 'DR') {
+                                                            console.log("dato: ",$scope.dataChicago[i]);
+
                                 $scope.dr = $scope.dr + Number($scope.dataChicago[i]._amount_paid);
                             }
                         }
