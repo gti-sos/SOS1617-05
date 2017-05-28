e24f045a9dae22fdc0be6b0926e4a973f82cde82
SOS1617_ipl <ipeluaga@gmail.com>
2017-05-28 01:54:09 +0000

    deploy to keroku

---
+++ public/frontends/nacho/chicagoCtrl.js
@@ -0,0 +1,75 @@
+/*global angular*/
+
+angular
+    .module("ManagerApp")
+    .controller("ChicagoCtrl", ["$http", "$scope", function($http, $scope) {
+
+        // http://jsfiddle.net/gh/get/library/pure/highcharts/highcharts/tree/master/samples/highcharts/demo/column-stacked-percent/
+        
+        //LA CUARTA (LA DE ZALANDO ES LA TERCERA) PUEDE SER LA DE CHICAGO (https://data.cityofchicago.org/resource/ydr8-5enu.json?) EN UN GRÁFICO DE BARRAS. Una barra para cada partido y despues una barra 
+        //para cada "algo" (por ejemplo: AVE, ST, BVD, RD, etc...) de los datos del json que muestre el gasto (por ejemplo) en ese algo.
+        //O también, dado que los recursos tienen fecha, mostrar los gastos en cada año.
+
+        //Spain Elections data (My API)
+        $scope.apikey = "cinco";
+        $scope.ppValue = 0;
+        $scope.podemosValue = 0;
+        $scope.psoeValue = 0;
+        $scope.csValue = 0;
+        $scope.data = {};
+        var data = {};
+
+        $scope.rpValue = 0;
+        $scope.dpValue = 0;
+        $scope.dataUs = {};
+        var dataUs = {};
+
+
+        $http
+            .get("/api/v1/elections-voting-stats?apikey=" + $scope.apikey)
+            .then(function(res) {
+
+                data = res.data;
+                $scope.data = data;
+                //data.sort(sort_by('province', true, parseInt));
+
+                for (var i = 0; i < res.data.length; i++) {
+                    $scope.ppValue = $scope.ppValue + Number($scope.data[i].pp);
+                    $scope.psoeValue = $scope.psoeValue + Number($scope.data[i].psoe);
+                    $scope.podemosValue = $scope.podemosValue + Number($scope.data[i].podemos);
+                    $scope.csValue = $scope.csValue + Number($scope.data[i].cs);
+                }
+
+                $http
+                    .get("https://data.cityofchicago.org/resource/ydr8-5enu.json?") //AL ACCEDER A EST URL ME DICE: 301 Moved Permanently !!!!!!
+                    .then(function(res) {
+
+                        dataUs = res.data;
+                        $scope.dataUs = dataUs;
+
+                        for (var i = 0; i < res.data.length; i++) { //HAY ALGÚN PARTIDO QUE NO SEA ALGUNO DE ESTOS 2 EN EL JSON QUE SE DEVUELVE???
+                            //console.log("Entra al bucle usData...PARTY: ",$scope.dataUs[i][7]);
+                            console.log("HAY DATO EN BUCLE usData?: ",$scope.dataUs[i]); //DEVUELVE HTML!!!!! COMPROBAR PROXY????
+                            if ($scope.dataUs[i]["party"] == "DEM") {
+                                $scope.dpValue = $scope.dpValue + 1;
+                            }
+                            else if ($scope.dataUs[i]['party'] == "REP") {
+                                $scope.rpValue = $scope.rpValue + 1;
+                            }
+                        }
+
+                        //Quizas no los representa porque son número muy grandes...PROBAR CON PORCENTAJES!!!
+                        console.log("Controller intialized. Values... ");
+                        console.log("PP: ", $scope.ppValue);
+                        console.log("PSOE: ", $scope.psoeValue);
+                        console.log("C's: ", $scope.csValue);
+                        console.log("PODEMOS: ", $scope.podemosValue);
+                        console.log("DEM: ", $scope.dpValue);
+                        console.log("REP: ", $scope.rpValue);
+
+                        //Posibles librerías: morris.js , chartist.js , 
+                        //También se podría usar una cosa como: http://plottablejs.org/examples/mondrian/ así viendo la diferencia entre las areas se apreciaría de anera muy gráfica la diferencia en volumen que hay entre unas elecciones aquí y unas alli.
+
+                    });
+            });
+    }]);
